38d09f11fe2f73aaa89db0fe637c1b53cce084fc
Yasser <yasserbal@icloud.com>
2022-05-30 09:14:57 +0200

    Added dark mode

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+
+
+
+//-----SECTION 5-----
+//RANDOM LETTERS
 Array.from(document.querySelectorAll(".letter")).forEach(el => {
    el.innerText = randomLetter();
 });
@@ -6,3 +12,16 @@ function randomLetter(){
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+
+
+//DARK-MODE
+const btnMode = document.getElementById('modeSwitch');
+btnMode.addEventListener('click',()=>{
+    if(document.body.classList.contains('lightMode')){
+    document.body.classList.remove('lightMode');
+    document.body.classList.add('darkMode')
+}else{
+    document.body.classList.remove('darkMode');
+    document.body.classList.add('lightMode')
+}
+})
