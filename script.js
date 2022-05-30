378d8308bddec063d7ce91453270b97dc0ae44db
Yasser <yasserbal@icloud.com>
2022-05-30 09:44:58 +0200

    Added parralax-carousel

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
+//----SECTION 1----
+//CAROUSEL
+//SOLUTION 2;
+const carouselImageURLs = ['./assets/carousel/image1.webp','./assets/carousel/image2.jpg','./assets/carousel/image3.webp','./assets/carousel/image4.jpg'];
+const carouselContainer = document.getElementById('carouselContainer');
+let activeImageIndex = 0;
+carouselContainer.style.backgroundImage = `url(${carouselImageURLs[activeImageIndex]})`
+carouselContainer.style.backgroundRepeat = `no-repeat`
+carouselContainer.style.backgroundSize = 'cover';
+carouselContainer.style.backgroundAttachment = 'fixed';
+setInterval(carouselImageScroll,1000)
+function carouselImageScroll(){
+    if(activeImageIndex<carouselImageURLs.length-1){
+        activeImageIndex++
+        carouselContainer.style.backgroundImage = `url(${carouselImageURLs[activeImageIndex]})`
+    }else{
+        activeImageIndex = 0;
+        carouselContainer.style.backgroundImage = `url(${carouselImageURLs[activeImageIndex]})`
 
+    }
 
+}
+let foregroundImage = new Image();
+foregroundImage.src = './assets/carousel/mario.webp';
+carouselContainer.appendChild(foregroundImage);
+foregroundImage.style.position = 'absolute';
+foregroundImage.style.bottom = '0';
+foregroundImage.style.left = '0';
+foregroundImage.style.width = '100%';
 
+//SOLUTION 1;
+// const carouselImageURLs = ['./assets/carousel/image1.webp','./assets/carousel/image2.jpg','./assets/carousel/image3.webp','./assets/carousel/image4.jpg'];
+// const carouselContainer = document.getElementById('carouselContainer');
+// let activeImageIndex = 0;
+// let foregroundImage = new Image();
+// foregroundImage.src = './assets/carousel/mario.webp';
+// carouselContainer.appendChild(foregroundImage);
+// foregroundImage.style.position = 'absolute';
+// foregroundImage.style.bottom = '0';
+// foregroundImage.style.left = '0';
+// foregroundImage.style.width = '100%';
+
+// let activeImage = new Image();
+// activeImage.src = './assets/carousel/image1.webp';
+// activeImage.style.backgroundRepeat = 'no-repeat';
+// activeImage.style.backgroundSize = 'cover';
+// activeImage.style.width = '100%';
+// carouselContainer.appendChild(activeImage);
+
+// setInterval(carouselImageScroll,1000)
+
+// function carouselImageScroll(){
+//     if(activeImageIndex<carouselImageURLs.length-1){
+//         activeImageIndex++;
+//         activeImage.src = carouselImageURLs[activeImageIndex];
+//     }else{
+//         activeImageIndex = 0;
+//         activeImage.src = carouselImageURLs[activeImageIndex];
+//     }
+// }
 
 //-----SECTION 5-----
 //RANDOM LETTERS
